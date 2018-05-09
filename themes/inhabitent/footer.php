9d668a28f26ede7e8fc17bd1677bc6c5f0024255
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-05-08 18:21:23 -0700

    Preliminar Foofer and Header

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -4,16 +4,42 @@
  *
  * @package RED_Starter_Theme
  */
-
 ?>
-
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
+				<!-- 				
+								<div class="site-info">
+									<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+								</div> -->
+				
+				<div class="site-contact">
+					<h2>Contact Info</h2>
+					<ul class="fa-ul">
+						<li><a class="email" href="mailto:info@inhabitent.com" rel="nofollow">
+							<span class="fa-li"><i class="fas fa-envelope"></i></span>info@inhabitent.com</a></li>
+						<li><a class="phone" href="tel:+17784567891" rel="nofollow">
+							<span class="fa-li"><i class="fas fa-phone" data-fa-transform="flip-h"></i></span>+1 778-456-7891</a></li>
+
+						<li><a class="hidden facebook" href="#" target="_black" rel="nofollow">
+							<span class="fa-li"><i class="fab fa-facebook-f"></i></span>http://facebook.com/inhabitent</a></li>
+						<li><a class="hidden twitter" href="#" target="_black" rel="nofollow">
+							<span class="fa-li"><i class="fab fa-twitter"></i></span>http://twitter.com/inhabitent</a></li>
+						<li><a class="hidden googlePlus" href="#" target="_black" rel="nofollow">
+							<span class="fa-li"><i class="fab fa-google-plus-g"></i></span>http://google.com/+/inhabitent</a></li>
+
+					</ul>
+				</div>
+
+				<div class="open-hours">
+					<h2>Business Hours</h2>
+					
+				</div>
+
+				
 			</footer><!-- #colophon -->
+
+
 		</div><!-- #page -->
 
 		<?php wp_footer(); ?>
