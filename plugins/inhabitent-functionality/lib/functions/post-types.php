f62edd7fb2b7f440ac50902d193a94725d81ee54
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-05-17 19:30:11 -0700

    Functionality Plug in

--- plugins/inhabitent-functionality/lib/functions/post-types.php
+++ plugins/inhabitent-functionality/lib/functions/post-types.php
@@ -5,4 +5,61 @@
  * @link  http://codex.wordpress.org/Function_Reference/register_post_type
  */
 
+ 
+
 // Add your custom post types here...
+// Register Custom Post Type
+function products_post_type() {
+
+	$labels = array(
+		'name'                  => _x( 'Products', 'Post Type General Name', 'inhabitent_text_domain' ),
+		'singular_name'         => _x( 'Product', 'Post Type Singular Name', 'inhabitent_text_domain' ),
+		'menu_name'             => __( 'Products', 'inhabitent_text_domain' ),
+		'name_admin_bar'        => __( 'Products', 'inhabitent_text_domain' ),
+		'archives'              => __( 'Archived Products', 'inhabitent_text_domain' ),
+		'attributes'            => __( 'Product Attributes', 'inhabitent_text_domain' ),
+		'parent_item_colon'     => __( 'Parent Product:', 'inhabitent_text_domain' ),
+		'all_items'             => __( 'All Products', 'inhabitent_text_domain' ),
+		'add_new_item'          => __( 'Add a Product', 'inhabitent_text_domain' ),
+		'add_new'               => __( 'Add New Product', 'inhabitent_text_domain' ),
+		'new_item'              => __( 'New Product', 'inhabitent_text_domain' ),
+		'edit_item'             => __( 'Edit Product', 'inhabitent_text_domain' ),
+		'update_item'           => __( 'Update Product', 'inhabitent_text_domain' ),
+		'view_item'             => __( 'View Product', 'inhabitent_text_domain' ),
+		'view_items'            => __( 'View Products', 'inhabitent_text_domain' ),
+		'search_items'          => __( 'Search Product', 'inhabitent_text_domain' ),
+		'not_found'             => __( 'Not found', 'inhabitent_text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'inhabitent_text_domain' ),
+		'featured_image'        => __( 'Main Product Image', 'inhabitent_text_domain' ),
+		'set_featured_image'    => __( 'Set main product image', 'inhabitent_text_domain' ),
+		'remove_featured_image' => __( 'Remove main product image', 'inhabitent_text_domain' ),
+		'use_featured_image'    => __( 'Use as main product image', 'inhabitent_text_domain' ),
+		'insert_into_item'      => __( 'Insert into Product', 'inhabitent_text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Product', 'inhabitent_text_domain' ),
+		'items_list'            => __( 'Products list', 'inhabitent_text_domain' ),
+		'items_list_navigation' => __( 'Products list navigation', 'inhabitent_text_domain' ),
+		'filter_items_list'     => __( 'Filter Products list', 'inhabitent_text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Product', 'inhabitent_text_domain' ),
+		'description'           => __( 'Products', 'inhabitent_text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array( 'title', 'editor', 'thumbnail' ),
+		'taxonomies'            => array( 'category', 'post_tag' ),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'products', $args );
+
+}
+add_action( 'init', 'products_post_type', 0 );
