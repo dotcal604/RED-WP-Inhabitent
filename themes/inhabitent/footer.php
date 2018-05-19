fd0a4bd4540ad7608c57ca39a2d375a3dd211bf2
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-05-19 11:20:34 -0700

    Upate Footer CSS

--- themes/inhabitent/footer.php
+++ themes/inhabitent/footer.php
@@ -12,39 +12,39 @@
 								<div class="site-info">
 									<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
 								</div> -->
-				
-				<div class="site-contact">
-					<h2>Contact Info</h2>
-					<ul class="fa-ul">
-						<li><a class="email" href="mailto:info@inhabitent.com" rel="nofollow">
-							<span class="fa-li"><i class="fas fa-envelope"></i></span>info@inhabitent.com</a></li>
-						<li><a class="phone" href="tel:+17784567891" rel="nofollow">
-							<span class="fa-li"><i class="fas fa-phone" data-fa-transform="flip-h"></i></span>+1 778-456-7891</a></li>
+				<div> 
+					<div class="site-contact">
+						<h2>Contact Info</h2>
+						<ul class="fa-ul">
+							<li><a class="email" href="mailto:info@inhabitent.com" rel="nofollow">
+								<span class="fa-li"><i class="fas fa-envelope"></i></span>info@inhabitent.com</a></li>
+							<li><a class="phone" href="tel:+17784567891" rel="nofollow">
+								<span class="fa-li"><i class="fas fa-phone" data-fa-transform="flip-h"></i></span>+1 778-456-7891</a></li>
+						</ul>
+						<ul class="fa-ul socialNetwork">
+							<li><a class="facebook" href="#" target="_black" rel="nofollow">
+								<span class="fa-li"><i class="fab fa-facebook-f"></i></span>http://facebook.com/inhabitent</a></li>
+							<li><a class="twitter" href="#" target="_black" rel="nofollow">
+								<span class="fa-li"><i class="fab fa-twitter"></i></span>http://twitter.com/inhabitent</a></li>
+							<li><a class="googlePlus" href="#" target="_black" rel="nofollow">
+								<span class="fa-li"><i class="fab fa-google-plus-g"></i></span>http://google.com/+/inhabitent</a></li>
+						</ul>
+					</div>
 
-						<li><a class="hidden facebook" href="#" target="_black" rel="nofollow">
-							<span class="fa-li"><i class="fab fa-facebook-f"></i></span>http://facebook.com/inhabitent</a></li>
-						<li><a class="hidden twitter" href="#" target="_black" rel="nofollow">
-							<span class="fa-li"><i class="fab fa-twitter"></i></span>http://twitter.com/inhabitent</a></li>
-						<li><a class="hidden googlePlus" href="#" target="_black" rel="nofollow">
-							<span class="fa-li"><i class="fab fa-google-plus-g"></i></span>http://google.com/+/inhabitent</a></li>
+					<div class="open-hours">
+						<h2>Business Hours</h2>
+						<div>
+							<p><span>Monday-Friday: </span>9am to 5pm</p>
+							<p><span>Saturday: </span>10am to 2am</p>
+							<p><span>Sundays: </span>Closed</p>
+						</div>
+					</div>
 
-					</ul>
+					<div class="logo">
+						<div class="screen-reader-text">Inhabitent Company Supply Co.</div>
+					</div>
 				</div>
-
-				<div class="open-hours">
-					<h2>Business Hours</h2>
-					<div>
-                        <p><span>Monday-Friday: </span>9am to 5pm</p>
-                        <p><span>Saturday: </span>10am to 2am</p>
-                        <p><span>Sundays: </span>Closed</p>
-                    </div>
-				</div>
-
-                <aside class="logo">
-                    <div class="screen-reader-text">Inhabitent Company Supply Co.</div>
-                </aside>
-
-                <div>
+                <div class="copyright">
                     Copyright <i class="far fa-copyright"></i> 2016 Inhabitent
                 </div>
 				
