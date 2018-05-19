142c82deac503b964ee96d06063fcf4f141f137c
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-05-19 14:14:44 -0700

    Style the header

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -5,7 +5,7 @@
  * @package RED_Starter_Theme
  */
 
-?><!DOCTYPE html>
+?><!DOCTYPE html> 
 <html <?php language_attributes(); ?>>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -23,7 +23,7 @@
 			<header id="masthead" class="site-header" role="banner">
 				<div class="site-branding">
 					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+					<p class="site-description"><span class="visuallyhidden"><?php bloginfo( 'description' ); ?></span></p>
 				</div><!-- .site-branding -->
 
 				<nav id="site-navigation" class="main-navigation" role="navigation">
