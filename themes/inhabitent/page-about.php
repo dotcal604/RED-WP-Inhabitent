35a212945e728a7633fb69a5e2007c48583a391c
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-05-23 11:35:39 -0700

    Update

---
+++ themes/inhabitent/page-about.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+    <div class="content-to-center">
+        <div id="primary" class="about-content-area">
+            <main id="main" class="site-main" role="main">
+
+                <?php while ( have_posts() ) : the_post(); ?>
+
+                    <?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+                <?php endwhile; // End of the loop. ?>
+
+            </main><!-- #main -->
+        </div><!-- #primary -->
+    </div>
+    
+<?php get_footer(); ?>
