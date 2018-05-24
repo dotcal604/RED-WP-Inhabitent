7208d2d644436a91206f3ec4a11f73ef9098b643
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-05-23 18:04:21 -0700

    Home Page Updates

--- themes/inhabitent/front-page.php
+++ themes/inhabitent/front-page.php
@@ -24,20 +24,18 @@ get_header(); ?>
             $product_types = get_terms( $query_args );
         ?>
 
-        <div class="">
         <?php if ( !empty( $product_types ) ) : ?>
-            <ul>
-            <?php foreach ( $product_types as $product_type ): ?>
-                <li class="<?php "product-type-".$product_type->slug ?>">
-                    <h3> <?php echo $product_type->name; ?> </h3>
-                    <img src= <?php echo esc_url( get_template_directory_uri() ) . "/build/img/product-type-icons/" . $product_type->slug . ".svg"; ?> />
-                    <p> <?php echo $product_type->description; ?> </p>
-                    <a href=" <?php echo get_term_link( $product_type ); ?> " class=""><?php echo $product_type->name . " stuff"; ?></a>
-                </li>
-            <?php endforeach; ?>
-            </ul>
+        <ul class="shop-stuff">
+        <?php foreach ( $product_types as $product_type ): ?>
+            <li class="<?php "product-type-".$product_type->slug ?>">
+                <h3 class="screen-reader-text"> <?php echo $product_type->name; ?> </h3>
+                <img src= <?php echo esc_url( get_template_directory_uri() ) . "/build/img/product-type-icons/" . $product_type->slug . ".svg"; ?> />
+                <p> <?php echo $product_type->description; ?> </p>
+                <p> <a href=" <?php echo get_term_link( $product_type ); ?> " class="btn"><?php echo $product_type->name . " stuff"; ?></a> </p>
+            </li>
+        <?php endforeach; ?>
+        </ul>
         <?php endif; ?>
-        </div>
     </session>
 
     <session class="page-content journal">
@@ -52,20 +50,18 @@ get_header(); ?>
             $blog_posts = get_posts( $query_args ); // returns an array of posts
         ?>
 
-        <div class="">
         <?php if ( !empty( $blog_posts ) ) : ?>
-            <ul class="">
-            <?php foreach ( $blog_posts as $post ) : setup_postdata( $post ); ?>
-                <li>
-                    <div class=""><?php the_post_thumbnail( 'medium_large' ); ?></div>
-                    <div class=""><?php red_starter_posted_on(); ?> / <?php red_starter_comment_count(); ?></div>
-                    <h3 class=""><a href="<?php the_permalink(); ?>" class=""><?php the_title(); ?></a></h3>
-                    <a href="<?php the_permalink(); ?>" class="">Read entry</a>
-                <li>
-            <?php endforeach; wp_reset_postdata(); ?>
-            </ul>
+        <ul class="journal">
+        <?php foreach ( $blog_posts as $post ) : setup_postdata( $post ); ?>
+            <li>
+                <div class="picture"><?php the_post_thumbnail( 'medium_large' ); ?></div>
+                <div class="info"><?php red_starter_posted_on(); ?> / <?php red_starter_comment_count(); ?></div>
+                <h3 class="title"><a href="<?php the_permalink(); ?>" class=""><?php the_title(); ?></a></h3>
+                <a href="<?php the_permalink(); ?>" class="btn">Read entry</a>
+            </li>
+        <?php endforeach; wp_reset_postdata(); ?>
+        </ul>
         <?php endif; ?>
-        </div>
     </session>
 
     <session class="page-content adventures">
@@ -80,22 +76,17 @@ get_header(); ?>
             $adventures_posts = get_posts( $query_args ); // returns an array of posts
         ?>
 
-        <div class="">
         <?php if ( !empty( $adventures_posts ) ) : ?>
-            <ul class="">
-            <?php foreach ( $adventures_posts as $post ) : setup_postdata( $post ); ?>
-                <li>
-                    <div class=""><?php the_post_thumbnail( 'medium_large' ); ?></div>
-                    <div class=""><?php red_starter_posted_on(); ?> / <?php red_starter_comment_count(); ?></div>
-                    <h3 class=""><a href="<?php the_permalink(); ?>" class=""><?php the_title(); ?></a></h3>
-                    <a href="<?php the_permalink(); ?>" class="">Read entry</a>
-                <li>
-            <?php endforeach; wp_reset_postdata(); ?>
-            </ul>
+        <ul class="adventures">
+        <?php foreach ( $adventures_posts as $post ) : setup_postdata( $post ); ?>
+            <li>
+                <div class="picture"><?php the_post_thumbnail( 'medium_large' ); ?></div>
+                <h3 class="title"><a href="<?php the_permalink(); ?>" class=""><?php the_title(); ?></a></h3>
+            </li>
+        <?php endforeach; wp_reset_postdata(); ?>
+        </ul>
         <?php endif; ?>
-        </div>
     </session>
-
 
 </div>
 
